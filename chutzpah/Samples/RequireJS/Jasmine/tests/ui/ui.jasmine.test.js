97e355a8695295f998266b5f0f2a9b4e3c489a77
Regina Kandiah <reginakandiah.cv@gmail.com>
2017-02-09 14:22:55 -0500

    deleted unnecessary files from chutzpah, jquery, and qunit

--- chutzpah/Samples/RequireJS/Jasmine/tests/ui/ui.jasmine.test.js
+++
@@ -1,9 +0,0 @@
-﻿define(['ui/screen'],
-    function (screen) {
-        describe("ui/screen", function () {
-            it("will build display version", function () {
-                var disp = screen.displayVersion;
-                expect(disp).toEqual("Version: 8");
-            });
-        });
-    });
