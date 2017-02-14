d089653152e4ef4d0e8e01c587b313c8a7e9354b
Corey Levine <coreylevine123@gmail.com>
2017-02-14 15:52:35 -0500

    Testing environment

--- src/app/config.js
+++ src/app/config.js
@@ -1,12 +1,12 @@
 module.exports = {
 
-  // Change this to your firebase configuration! (Add Firebase to your web app)
+    // Change this to your firebase configuration! (Add Firebase to your web app)
     FIREBASE_CONFIG: {
-    apiKey: "AIzaSyD8aGSZqtEG1J4Spw7L_0Qe1f3GAJmuwqw",
-    authDomain: "concorde-b6468.firebaseapp.com",
-    databaseURL: "https://concorde-b6468.firebaseio.com",
-    storageBucket: "concorde-b6468.appspot.com",
-    messagingSenderId: "380387826469"
+        apiKey: 'AIzaSyD8aGSZqtEG1J4Spw7L_0Qe1f3GAJmuwqw',
+        authDomain: 'concorde-b6468.firebaseapp.com',
+        databaseURL: 'https://concorde-b6468.firebaseio.com',
+        storageBucket: 'concorde-b6468.appspot.com',
+        messagingSenderId: '380387826469',
     },
 };
 
