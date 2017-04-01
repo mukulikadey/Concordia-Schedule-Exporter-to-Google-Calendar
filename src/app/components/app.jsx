9e77a10c7959926187c7886ec2d511b8d0905eec
Samrat Debroy <samdebroy@gmail.com>
2017-04-01 17:51:31 -0400

    Revert "Issue: #124 Removed dropdown menu from nav bar and made all items directly on menu. Replaced title of website with logo. Also removed buttons at the bottom of the homepage since the links are now always displayed on each page in the nav menu. Also changed the colours of the menu to try to match it with the rest of the site."
    This reverts commit 827cec31efd250593a755e596da15ccd0ade1dab.

--- src/app/components/app.jsx
+++ src/app/components/app.jsx
@@ -3,103 +3,98 @@ import { browserHistory, Link } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchUser, logoutUser,getEvents } from '../actions/firebase_actions';
-import './app.scss';
-
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.props.fetchUser();
-    //this.props.getEvents();
-    this.logOut = this.logOut.bind(this);
-  }
-
-  logOut() {
-    this.props.logoutUser().then((data) => {
+    constructor(props) {
+        super(props);
+        this.props.fetchUser();
+        this.logOut = this.logOut.bind(this);
+    }
+ 
+    logOut() {
+        this.props.logoutUser().then((data) => {
       // reload props from reducer
-      this.props.fetchUser();
-    });
-  }
+            this.props.fetchUser();
+            gapi.auth2.getAuthInstance().signOut();
+        });
+    }
 
-  renderUserMenu(currentUser) {
+    renderUserMenu(currentUser) {
     // if current user exists and user id exists than make user navigation
-    if (currentUser && currentUser.uid) {
-      this.props.getEvents()
-      return (
-        <li className="dropdown">
-          <a
-            href="#" className="dropdown-toggle" data-toggle="dropdown" role="button"
-            aria-haspopup="true" aria-expanded="false"
-          >
-            {currentUser.email} <span className="caret" /></a>
-          <ul className="dropdown-menu">
-            <li><Link to="/profile"><span className="fa fa-user" aria-hidden="true"></span> Profile</Link></li>
-            <li role="separator" className="divider" />
-            <li><Link to="/scheduleGen"><span className="fa fa-calendar" aria-hidden="true"></span> Schedule</Link></li>
-            <li role="separator" className="divider" />
-            <li><Link to><span className="fa fa-comments" aria-hidden="true"></span> Forum</Link></li>
-            <li role="separator" className="divider" />
-            <li><Link to="/login" onClick={this.logOut}> <span className="fa fa-sign-out" aria-hidden="true"></span> Logout</Link></li>
-          </ul>
-        </li>
-      );
-    } else {
-      return [
-        <li key={1}><Link to="/login">Login/Register</Link></li>,
-      ];
+        if (currentUser && currentUser.uid) {
+            return (
+                <li className="dropdown">
+                    <a
+                      href="#" className="dropdown-toggle" data-toggle="dropdown" role="button"
+                      aria-haspopup="true" aria-expanded="false"
+                    >
+                        {currentUser.email} <span className="caret" /></a>
+                    <ul className="dropdown-menu">
+                        <li><Link to="/profile"><span className="fa fa-user" aria-hidden="true"></span> Profile</Link></li>
+                        <li role="separator" className="divider" />
+                        <li><Link to="/scheduleGen"><span className="fa fa-calendar" aria-hidden="true"></span> Schedule</Link></li>
+                        <li role="separator" className="divider" />
+                        <li><Link to="/login" onClick={this.logOut}> <span className="fa fa-sign-out" aria-hidden="true"></span> Logout</Link></li>
+                    </ul>
+                </li>
+            );
+        } else {
+            return [
+                <li key={1}><Link to="/login">Login/Register</Link></li>,
+            ];
+        }
     }
-  }
 
-  render() {
-    var homeLink ;
-    if (this.props.currentUser) {
+    render() {
+      var homeLink ;
+      if (this.props.currentUser)
+        homeLink = "/index_home";
+      else
+        homeLink = "/login";
 
-      homeLink = "/index_home";
-    }
-    else
-      homeLink = "/login";
+        return (
+            <div>
+                <header className="navbar navbar-static-top navbar-inverse" id="top" role="banner">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button
+                              className="navbar-toggle collapsed" type="button" data-toggle="collapse"
+                              data-target=".bs-navbar-collapse"
+                            ><span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar" />
+                                <span className="icon-bar" />
+                                <span className="icon-bar" />
+                            </button>
+                            <Link to={homeLink} className="navbar-brand">Concordia University Schedule Exporter (C.U.S.E)</Link>
+                        </div>
+                        <nav className="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+                            <ul className="nav navbar-nav navbar-right">
+                                { this.renderUserMenu(this.props.currentUser) }
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
 
-    return (
-      <div>
-        <header className="navbar navbar-custom"  role="banner">
-          <div className="container">
-            <div className="navbar-header">
-              <Link to={homeLink} className="navbar-brand"><img className="img" src="/src/logo.png" height={45} /></Link>
+                <div className="container">
+                    {this.props.children}
+
+
+
+
+                </div>
             </div>
-            <ul className="nav navbar-nav">
-              <li><Link to="/profile" ><span className="fa fa-user" aria-hidden="true"></span> Profile</Link></li>
-              <li><Link to="/scheduleGen"><span className="fa fa-calendar" aria-hidden="true"></span> Schedule</Link></li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li><Link to="/login" onClick={this.logOut}> <span className="fa fa-sign-out" aria-hidden="true"></span> Logout</Link></li>
-
-
-            </ul>
-
-          </div>
-        </header>
-
-        <div className="container">
-          {this.props.children}
-
-
-
-
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchUser, logoutUser, getEvents }, dispatch);
+    return bindActionCreators({ fetchUser, logoutUser, getEvents }, dispatch);
 }
 
 
 function mapStateToProps(state) {
-  return { currentUser: state.currentUser };
+    return { currentUser: state.currentUser };
 }
 
 
