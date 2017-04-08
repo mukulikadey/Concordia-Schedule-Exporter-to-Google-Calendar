644e8e27fbb24baef05032a3bb7011a8fbcba9a2
Corey Levine <coreylevine123@gmail.com>
2017-04-08 18:50:11 -0400

    #108 - Added test for checkweekday

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -60,6 +60,45 @@ describe('populate', () => {
   });
 });
 
+describe('Weekdays', () => {
+  let startDate = new Date(2017, 1, 9);
+  let endDate = new Date(2017, 4, 13);
+  let noClassThisDay = new Array();
+  FireBaseTools.fillNoClassThisDay(noClassThisDay);
+  let val = () => { return {
+      'COMP346Y YA': {
+      'Term': 2164,
+      'Subject': 'COMP',
+      'Catalog': 346,
+      'Section': 'Y YA',
+      'Component': 'TUT',
+      'Pat Nbr': 1,
+      'Room Nbr': '',
+      'Mtg Start': '8:30:00 AM',
+      'Mtg End': '9:20:00 AM',
+      'Mon': 'N',
+      'Tues': 'Y',
+      'Wed': 'N',
+      'Thurs': 'N',
+      'Fri': 'N',
+      'Sat': 'N',
+      'Sun': 'N',
+      'Start Date': '2017-01-09',
+      'End Date': '2017-04-13',
+      'Last': 'Goswami',
+      'First Name': 'Dhrubajyoti',
+      'Email': 'dhrubajyoti.goswami@concordia.ca'
+      }
+    }
+  }  
+  let snap = {val: val};
+  FireBaseTools.checkWeekday(startDate, endDate, snap);
+
+  it('check weekday', () => {
+    expect(FireBaseTools.checkWeekday(startDate, endDate, snap)).to.exist;
+  });
+});
+
 describe('setDateEvents', () => {
 
   let course = {
