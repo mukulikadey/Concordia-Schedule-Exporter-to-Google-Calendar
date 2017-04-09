50ee4273dd8eaa33764d4274c5953d5b948bacf2
Corey Levine <coreylevine123@gmail.com>
2017-04-08 20:12:14 -0400

    #108 - Rearranged order in which tests are executed.

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -4,16 +4,54 @@ import FireBaseTools from '../../src/app/utils/firebase';
 
 describe('fillNoClassThisDay', () => {
   let noClassThisDay = new Array();
-
   FireBaseTools.fillNoClassThisDay(noClassThisDay);
+
+  it('expect array', () => {
+    expect(noClassThisDay).to.be.a('array');
+  });
   it('fill', () => {
       expect(noClassThisDay.length).greaterThan(0);
   });
   it('number of days', () => {
     expect(noClassThisDay.length).equals(11);
   });
-    it('expect array', () => {
-    expect(noClassThisDay).to.be.a('array');
+});
+
+describe('Weekdays', () => {
+  let startDate = new Date(2017, 1, 9);
+  let endDate = new Date(2017, 4, 13);
+  let noClassThisDay = new Array();
+  FireBaseTools.fillNoClassThisDay(noClassThisDay);
+  let val = () => { return {
+      'COMP346Y YA': {
+      'Term': 2164,
+      'Subject': 'COMP',
+      'Catalog': 346,
+      'Section': 'Y YA',
+      'Component': 'TUT',
+      'Pat Nbr': 1,
+      'Room Nbr': '',
+      'Mtg Start': '8:30:00 AM',
+      'Mtg End': '9:20:00 AM',
+      'Mon': 'N',
+      'Tues': 'Y',
+      'Wed': 'N',
+      'Thurs': 'N',
+      'Fri': 'N',
+      'Sat': 'N',
+      'Sun': 'N',
+      'Start Date': '2017-01-09',
+      'End Date': '2017-04-13',
+      'Last': 'Goswami',
+      'First Name': 'Dhrubajyoti',
+      'Email': 'dhrubajyoti.goswami@concordia.ca'
+      }
+    }
+  }  
+  let snap = {val: val};
+
+  it('check weekday', () => {
+    expect(FireBaseTools.checkWeekday(startDate, endDate, snap)).to.not.exist;
   });
 });
 
@@ -54,48 +92,8 @@ describe('populate', () => {
   it('populate timetable', () => {
     expect(timeTable).to.exist;
   });
-
-    it('timetable object', () => {
+  it('timetable object', () => {
       expect(timeTable).to.be.an('object');
-  });
-});
-
-describe('Weekdays', () => {
-  let startDate = new Date(2017, 1, 9);
-  let endDate = new Date(2017, 4, 13);
-  let noClassThisDay = new Array();
-  FireBaseTools.fillNoClassThisDay(noClassThisDay);
-  let val = () => { return {
-      'COMP346Y YA': {
-      'Term': 2164,
-      'Subject': 'COMP',
-      'Catalog': 346,
-      'Section': 'Y YA',
-      'Component': 'TUT',
-      'Pat Nbr': 1,
-      'Room Nbr': '',
-      'Mtg Start': '8:30:00 AM',
-      'Mtg End': '9:20:00 AM',
-      'Mon': 'N',
-      'Tues': 'Y',
-      'Wed': 'N',
-      'Thurs': 'N',
-      'Fri': 'N',
-      'Sat': 'N',
-      'Sun': 'N',
-      'Start Date': '2017-01-09',
-      'End Date': '2017-04-13',
-      'Last': 'Goswami',
-      'First Name': 'Dhrubajyoti',
-      'Email': 'dhrubajyoti.goswami@concordia.ca'
-      }
-    }
-  }  
-  let snap = {val: val};
-  FireBaseTools.checkWeekday(startDate, endDate, snap);
-
-  it('check weekday', () => {
-    expect(FireBaseTools.checkWeekday(startDate, endDate, snap)).to.not.exist;
   });
 });
 
